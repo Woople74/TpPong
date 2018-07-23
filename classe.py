04e793521b3c7e0adc4649347bebe6c9d9e06a4f
Woople <aitaddi.marwan@gmail.com>
2018-07-23 16:39:00 +0200

    Fin de la structure générale bugs a corriger

--- classe.py
+++ classe.py
@@ -2,39 +2,49 @@
     - Raquette
     - Balle """
 import pygame
+from pygame.locals import *
 from constantes import *
 
 class Raquette:
-    def __init__(x):
+    def __init__(self, x):
         self.x = x
         self.y = 295
         self.direction = RIEN
-        slef.image = pygame.image.load("Img/raquette.png").convert_alpha()
+        self.image = pygame.image.load("Img/raquette.png").convert_alpha()
         self.rect = Rect((self.x, self.y), (largeurP, hauteurP))
-    def moove(fenetre, img):
+    def moove(self, fenetre):
         self.y = VITESSE_RAQUETTE * self.direction  
         if self.y > hauteurP and self.y == 480 - hauteurP: 
-            fenetre.blit(img, (self.x, self.y))
-    def collide(balle)
+            fenetre.blit(self.image, (self.x, self.y))
+    def collide(self, balle):
         if self.rect.collide(balle) == True:
             return True
 
 class Balle:
-    def __init__():
+    def __init__(self, ecran: pygame.surface):
         self.image = pygame.image.load("Img/balle.png").convert_alpha()
         self.x = (largeurE/2) - 10
         self.y = (hauteurE/2) - 10
         self.vect = [VITESSE_BALLE, -VITESSE_BALLE]
         self.rect = Rect((self.x, self.y), (20,20))
-    def refresh(listraquette):
-        #Utiliser raquette.collide + la taille de l'écran pour déterminer si la balle doit rebondir
-        #Si on tape par le haut on change la direction en y (négatif) en ajoutant du hasard et on ajoute juste du hasard sur le x
-        #L'inverse si on tape sur les cotés (On peut faire raquette pour cotés et bors de l'écrans pour le haut car 
-        #si la raquette loupe la balle on perd la partie
-        if raquette.collide(self.rect) == True :
-            self.vect[0] = -self.rect[0] + randint(100,180)
-            self.vect[1] += randint(100,180) 
+        self.ecran = ecran
+    def refresh(self, raquette1, raquette2):
+        if raquette1.collide(self.rect) == True :
+            self.vect[0] = -self.rect[0] + (randint(100,180) / 1000)
+            self.vect[1] += (randint(100,180) / 1000) 
+        if raquette2.collide(self.rect) == True :
+            self.vect[1] = -self.rect[1] + (randint(100,180) / 1000)
+            self.vect[0] += (randint(100,180) / 1000) 
         if self.y < 0 or self.y > largeurE - 20:
             self.vect[0]
         self.x += self.vect[0]
         self.y += self.vect[1]
+
+class Game:
+    def prepare(self, balle):
+        """Méthode qui se charge de tout remettre a 0 si besoin"""
+        balle.vect = [VITESSE_BALLE, -VITESSE_BALLE]
+        balle.x = (largeurE/2) - 10
+        balle.y = (hauteurE/2) - 10
+        score_1 = 0
+        score_2 = 0
--- pong.py
+++ pong.py
@@ -16,10 +16,36 @@ while continuer == 1:
     while menu == 1:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
+                raquette1 = Raquette(20)
+                raquette2 = Raquette(largeurE - 20)
+                balle = Balle(fenetre)
+                game = Game()
                 menu = 0
                 jeu = 1
+                game.prepare(balle)
     while jeu == 1:
+        """Appele toutes les méthodes de Game dans cet ordre :
+            -Prepare
+            boucle while :
+                boucle for des évenements
+                update
+                déplacement de la balle
+                pygame.display.flip"""
+        fenetre.blit(fond, (0,0))
         for event in pygame.event.get():
-            if event.type == KEYDOWN and event.key == K_ESCAPE:
-                menu = 1
-                jeu = 0
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    menu = 1
+                    jeu = 0
+                if event.key == K_z:
+                    raquette1.moove(HAUT)
+                if event.key == K_s:
+                    raquette2.moove(BAS)
+                if event.key == K_UP:
+                    raquette2.moove(HAUT)
+                if event.key == K_DOWN:
+                    raquette2.moove(BAS)
+        balle.refresh(raquette1, raquette2)
+        pygame.display.flip()
+
+
